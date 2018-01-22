bad80e771bd2441c204271ff1088c93123478cd6
rlee <robinleesuns13@gmail.com>
2018-01-21 16:17:37 -0800

    fixing half columns and some text edits

--- static/js/navshrink.js
+++ static/js/navshrink.js
@@ -2,27 +2,29 @@ $(document).ready(function () {
     var scroll_start = 0
     var startchange = $('#banner-back');
     var offset = startchange.offset();
-     if (startchange.length){
-         $(document).scroll(function(){
-             scroll_start = $(this).scrollTop();
-             if(scroll_start > offset.top){
-                 $('.navbar-default').css('background-color', '#fff')
-                 $('.navbar-nav li a').css('opacity', '0.6')
-                 $('.navbar-nav li a').css('color', '#000000')
-             } else{
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.navbar-default').css('background-color', '#fff')
+                $('.navbar-nav li a').css('opacity', '0.6')
+                $('.navbar-nav li a').css('color', '#000000')
+            } else {
                 $('.navbar-default').css('background-color', 'transparent')
-                 $('.navbar-nav li a').css('color', '#fff')
-             }
-         });
+                $('.navbar-nav li a').css('color', '#fff')
+            }
+        });
 
-     }
+    }
 });
 
-$('a[href^="#"]').on('click', function(event) {
+$('a[href^="#"]').on('click', function (event) {
     var target = $(this.getAttribute('href'));
     var navHeight = parseInt($('#menuBar').css('height'));
-    if( target.length ) {
+    if (target.length) {
         event.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: target.offset().top-navHeight
-        }, 700);}});
+            scrollTop: target.offset().top - navHeight
+        }, 700);
+    }
+});
--- yorodesign.py
+++ yorodesign.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template
 from flask_mail import Mail
 
-
 app = Flask(__name__)
 mail = Mail(app)
 
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
